c29c77148b4db4420507f116dd51f786760bc595
jinInoue-rrr <inojin0326@icloud.com>
2022-11-17 11:05:37 +0900

    ch2 initial commit

---
+++ ch2/rpoiss_n_large.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+n = 100000
+lambda = 2.1
+
+df <- tibble(y = rpois(n= n, lambda = lambda))
+
+gg <- df %>% 
+  ggplot(aes(x = y)) + 
+  geom_histogram(bins = 100) + 
+  theme_bw()
+
+ggsave(filename = "figure_2.1_left.png", plot = gg)
+
+
+
+
